93cfaf93a9eda805453ba319f9d443050d7fc67f
Jacob Januszanis <Jjanuszanis94@gmail.com>
2021-05-28 13:12:21 -0600

    Updated project section text/images

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import "../styles/projects.scss";
 import weatherAppImg from '../styles/images/weather-app-img.png';
-
+import pomoTimerImg from '../styles/images/pomoTimerApp.png';
 
 export default function Projects() {
     return (
@@ -14,9 +14,9 @@ export default function Projects() {
                         <h2 className="projects-flex-container__left-title">Weather App</h2>
                         <h3 className="projects-flex-container__left-subtitle">React, Tailwind CSS, Express, Heroku</h3>
                         <p className="projects-flex-container__left-description">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dictum in nulla nec ultricies.
-                        Ut mattis mollis tortor. Praesent commodo sit amet turpis lacinia euismod. Suspendisse erat lectus, sagittis nec quam quis, egestas venenatis enim.
-                        Donec fringilla congue sapien vitae mollis. Sed id ante mattis, facilisis risus non, aliquet massa. 
+                        Weather app that takes 5 digit US zip code and retrieves data from OpenWeatherMap API.
+                        The app displays either the current forecast or gives the user the option for a 5 day extended forecast.
+                        The frontend was built with React and Tailwind CSS while handling the API calls with an Express server on the backend. 
                         </p>
                         <div className="projects-flex-container__left-links">
                             <a href="https://github.com/Jake-Januszanis/react-weather" className="projects-flex-container__left-links-btn" target="_blank">Source Code</a>
@@ -33,9 +33,10 @@ export default function Projects() {
                         <h2 className="projects-flex-container__left-title">Pomodoro Timer</h2>
                         <h3 className="projects-flex-container__left-subtitle">HTML5, SASS, JavaScript, EJS, Express, Mongoose, MongoDB, Heroku</h3>
                         <p className="projects-flex-container__left-description">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dictum in nulla nec ultricies.
-                        Ut mattis mollis tortor. Praesent commodo sit amet turpis lacinia euismod. Suspendisse erat lectus, sagittis nec quam quis, egestas venenatis enim.
-                        Donec fringilla congue sapien vitae mollis. Sed id ante mattis, facilisis risus non, aliquet massa. 
+                        Pomodoro timer that allows users to keep track of time spent studying using the Pomodoro technique.
+                         Users can either use the guest timer or create an account which then allows them to save data to a 
+                         MongoDB database to compare results for future analytics.
+                         For security purposes, all passwords are hashed and salted using the bcrypt npm library.  
                         </p>
                         <div className="projects-flex-container__left-links">
                             <a href="https://github.com/Jake-Januszanis/pomodoro-timer" className="projects-flex-container__left-links-btn" target="_blank"> Source Code</a>
@@ -43,7 +44,7 @@ export default function Projects() {
                         </div>
                     </div>
                     <div className="projects-flex-container__right">
-                        <img className="projects-flex-container__right-image" src={weatherAppImg}></img>
+                        <img className="projects-flex-container__right-image-2" src={pomoTimerImg}></img>
                     </div>
                 </div>
             </div>
