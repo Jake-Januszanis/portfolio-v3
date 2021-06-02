86cf8e15f7333e8c28aeab1d813026f65dccf12b
Jacob Januszanis <Jjanuszanis94@gmail.com>
2021-06-02 11:16:13 -0600

    Updated html file to include netlify form attribute

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,7 +7,7 @@ export default function Contact() {
         <div className="contact-wrapper">
         <h1 className="contact__title">Contact</h1>
         <div className="contact__form-container">
-            <form className="contact__form">
+            <form className="contact__form" name="contact">
               
                 <div className="contact__form-group">
                     <input className="contact__form-input" type="text" id="name" name="name" placeholder=" " required></input>
