453287506380ea1b2786b33ec2c48ab3ceb928fb
Jacob Januszanis <Jjanuszanis94@gmail.com>
2021-06-05 16:35:15 -0600

    Refactored home.scss file/updated responsive media queries

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,7 +11,7 @@ export default function About() {
                     <div className="about-flex__left">
                        <p> Hello! I’m Jake Januszanis, a web developer currently located in Denver, CO.</p>
                         <p>Ironically my coding journey began nearly 3 years ago when I was out on a hike with a friend.
-                         He mentioned to me how he had been a freelance web developer for over 5 years and explained that it was something that anyone is capable of as long as they are committed.
+                         He mentioned how he had been a freelance web developer for over 5 years and explained that it was something that anyone is capable of as long as they were committed.
                          Both of us coming from a Brazilian Jiu-Jitsu background he told me “If you can learn Jiu-Jitsu then you can learn to code”.
                          </p>
                         <p>So that’s where it all began and since day one I knew that this is what I wanted to do for a career.
