1f2500821d2646d46647ec0b341040ef3ace00a3
Jake Januszanis <jakejanuszanis@Jakes-Air.attlocal.net>
2023-04-03 23:10:43 -0700

    Refactored navbar into styled components. Added small hover effect.

--- src/pages/components/Navbar.js
+++ src/pages/components/Navbar.js
@@ -1,7 +1,34 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import "../../styles/navbar.scss";
+import styled from 'styled-components';
 const resume = require("../../styles/images/Jake-Januszanis-Resume.pdf");
+
+const NavWrapper = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    margin: 2.5rem 2.5rem 0 0;
+    gap: 3rem;
+`
+const StyledLink = styled(Link)`
+    font-size: 2.5rem;
+    padding-right: 2.2rem;
+    text-decoration: none;
+    color: #222;
+    cursor: pointer;
+    transition: all ease-in .1s;
+    height: min-content;
+    background-color: transparent;
+    border: none;
+
+    &:hover {
+        font-weight: bold;
+        color: #0a838a;
+        transform: scale(1.1);
+        transition: all ease-in .1s;
+    }
+
+`
 
 export default function Navbar() {
 
@@ -17,12 +44,12 @@ export default function Navbar() {
 
 
     return (
-        <nav>
-            <Link className="nav__link" to="#about">About</Link>
-            <Link className="nav__link" to="#projects">Projects</Link>
-            <Link className="nav__link" to="#contact">Contact</Link>
-            <button className="nav__link nav__link-btn" onClick={openPdf} onKeyPress={handleKeyPress}>Resume</button>
-        </nav>
+        <NavWrapper>
+            <StyledLink to="#about">About</StyledLink>
+            <StyledLink to="#projects">Projects</StyledLink>
+            <StyledLink to="#contact">Contact</StyledLink>
+            <StyledLink as='button' onClick={openPdf} onKeyPress={handleKeyPress}>Resume</StyledLink>
+        </NavWrapper>
     )
 }
 
