396853370681b2d3f9f49c699bbd64dcea895359
Jake Januszanis <jakejanuszanis@Jakes-Air.attlocal.net>
2023-04-04 23:14:34 -0700

    Refactored home page styles into styled components. Removed old scss file.

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,28 +1,105 @@
 import React from 'react';
-import '../styles/home.scss';
 import Navbar from "../pages/components/Navbar";
 import ScrollBtn from "./components/Scroll-btn";
+import styled from 'styled-components';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
+
+const GREEN_ACCENT_COLOR = '#0a838a'
+const LARGE_SCREEN = '1750px'
+// const MED_SCREEN = '1200px'
+// const SMALL_SCREEN = '500px'
+
+
+const SectionWrapper = styled.section`
+  height: 100vh;
+  margin-bottom: 5rem;
+
+  @media(min-width: ${LARGE_SCREEN}) {
+    margin: 0 13%;
+  }
+`
+// const Circle = styled.div`
+//   height: 700px;
+//   width: 700px;
+//   border: none;
+//   border-radius: 100%;
+//   position: absolute;
+//   top: -500px;
+//   left: 800px;
+//   background-color: #0a838a;
+//   z-index: 1  
+// `
+
+const HomeWrapper = styled.div`
+  min-height: 100%;
+  position: relative;
+`
+const HomeHeaderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  row-gap: 5px;
+  position: absolute;
+  top: 20%;
+  margin-left: 15rem;
+`
+const HomePrimaryHeader = styled.h2`
+  font-size: 6rem;
+  color: ${GREEN_ACCENT_COLOR};
+  margin: 0;
+  padding: 0;
+
+  @media(min-width: ${LARGE_SCREEN}) {
+    font-size: 7rem;
+  }
+`
+
+const HomeSubHeader = styled(HomePrimaryHeader)`
+  color: #222;
+`
+
+const IconContainer = styled.div`
+  margin: 0;
+  padding: 0;
+  display: flex;
+  column-gap: 20px;
+  margin-top: 10px;
+`
+
+const HomeIcons = styled.a`
+  font-size: 4.5rem;
+  padding-right: 1rem;
+  color: #222;
+  cursor: pointer;
+  text-decoration: none;
+  transition: all ease-in-out .3s;
+
+  &:hover {
+    color: ${GREEN_ACCENT_COLOR};
+    font-size: 5rem;
+    transition: all ease-in-out .3s;
+  }
+`
+
 
 export default function Home() {
     return (
       
-    <section id="home">
-      <div className="home-wrapper">
+    <SectionWrapper id="home">
+      <HomeWrapper>
         <Navbar />
-        <div className="home-flex">
-          <div className="home-flex__1">
-            <h1 className="home-header-primary">Jake Januszanis</h1>
-            <h1 className="home-header-sub">Software Developer</h1>
-              <div className="home-icons__container">
-              <a href="https://github.com/Jake-Januszanis" target="_blank" rel="noreferrer" className="home-icons"><FaGithub /> </a>
-              <a href="https://www.linkedin.com/in/jake-januszanis/" target="_blank" rel="noreferrer" className="home-icons"><FaLinkedin /> </a>
-              </div>
-          </div>
-        </div>
+          <HomeHeaderContainer>
+            <HomePrimaryHeader>Jake Januszanis</HomePrimaryHeader>
+            <HomeSubHeader>Software Developer</HomeSubHeader>
+            <IconContainer>
+              <HomeIcons href="https://github.com/Jake-Januszanis" target="_blank" rel="noreferrer"><FaGithub /> </HomeIcons>
+              <HomeIcons href="https://www.linkedin.com/in/jake-januszanis/" target="_blank" rel="noreferrer"><FaLinkedin /> </HomeIcons>
+          </IconContainer>
+          {/* <Circle /> */} 
+          </HomeHeaderContainer>
         <a href="#projects"><ScrollBtn /> </a>
-      </div>
-    </section> 
+      </HomeWrapper>
+    </SectionWrapper> 
 
     )
 }
